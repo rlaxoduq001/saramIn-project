61a53acf6c832bdba2334764a50497035a447851
Yeop <rlaxoduq001@gmail.com>
2023-12-21 21:50:30 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex flex-row'>
+        <h1 className="basis-1/4">hi</h1>
+        <h1 className="basis-1/4">hi</h1>
+        <h1 className="basis-1/2">hi</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
